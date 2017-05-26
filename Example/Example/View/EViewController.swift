46ed58722f6ae051daaacd06ff213768dacca756
ApterKing <wangcccong@outlook.com>
2017-05-26 13:54:14 +0800

    修改View

--- Example/Example/View/EViewController.swift
+++ Example/Example/View/EViewController.swift
@@ -8,24 +8,23 @@
 
 import UIKit
 
-class EViewController: UIViewController {
+public class EViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.title = "组件E"
-        
+
         let label = UILabel(frame: CGRect(x: (UIScreen.main.bounds.size.width - 120) / 2.0, y: 200, width: 120, height: 50))
         label.text = "这里是组件E测试"
         self.view.addSubview(label)
     }
 
-    override func didReceiveMemoryWarning() {
+    public override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
--- Verify+E.podspec
+++ Verify+E.podspec
@@ -3,7 +3,7 @@ Pod::Spec.new do |s|
 
   # ―――  Spec Metadata  ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
   s.name         = "Verify+E"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "组件E"
 
   s.homepage     = "https://github.com/ApterKingRepo/Verify-E"
